a547c877542a723f1ac0293be55f728c11d9323b
Igor Ubuntu <iiiliev.igor@gmail.com>
2023-01-30 17:21:34 +0200

    Тестируем отправку сообщений по апи через реквест модуль

--- api_request.py
+++ api_request.py
@@ -1,7 +1,6 @@
 import requests
 import time
 
-
 API_URL: str = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = '5922136610:AAHpecQUXLSkuJDoglLXLOtg1xGgXyIJmbY'
 TEXT: str = 'Ура! Классный апдейт!'
@@ -25,3 +24,34 @@ while counter < MAX_COUNTER:
             requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
 
     time.sleep(1)
+
+
+
+
+##if updates['result']
+# {
+#   "ok": true,
+#   "result": [
+#     {
+#       "update_id": 792864383,
+#       "message": {
+#         "message_id": 32,
+#         "from": {
+#           "id": 413281115,
+#           "is_bot": false,
+#           "first_name": "...",
+#           "username": "username22549",
+#           "language_code": "ru"
+#         },
+#         "chat": {
+#           "id": 413281115,
+#           "first_name": "...",
+#           "username": "username22549",
+#           "type": "private"
+#         },
+#         "date": 1675091908,
+#         "text": "\u043f\u0440\u0438\u0432\u0435\u0442"
+#       }
+#     }
+#   ]
+# }
