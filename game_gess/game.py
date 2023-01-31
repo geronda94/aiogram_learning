7bb4d4d62e32ce35066b78f20856275d1530b9af
Igor Ubuntu <iiiliev.igor@gmail.com>
2023-01-31 22:02:58 +0200

    loong pooling timeout

---
+++ game_gess/game.py
@@ -0,0 +1,14 @@
+import random
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Text, Command
+from data import TOKEN
+
+
+bot: Bot = Bot(TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+
+
+
